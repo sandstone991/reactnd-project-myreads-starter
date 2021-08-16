1c1ab8a3333ba099d9269672490fd2738ab20ac3
Ahmed Mahmoud Azzam <gamerazzam@gmail.com>
2021-08-16 11:16:47 +0200

    done routing

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import './App.css'
 import ChangeShelf from './changeShelf'
+import img from './icons/thumbnail.jpg'
 export default function BookSearch(props){
 
   return(
-    props.bookList.map(book=> (
+    props.bookList.map(book=>{ 
+      return (
+
                     <li key={book.id}>
                      <div className="book" >
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-cover"
+                             style={
+                               { width: 128, height: 193, backgroundImage:`url(${typeof book.imageLinks=='undefined'?img:book.imageLinks.thumbnail})` }
+                               }
+                               >
+
+                            </div>
                            <ChangeShelf bookID={book} bookChange={props.bookChange}/>
                           </div>
                           <div className="book-title">{book.title}</div>
@@ -20,7 +29,7 @@ export default function BookSearch(props){
 
       
     )
-        
+    }
         
         )
 )
